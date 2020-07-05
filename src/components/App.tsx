47426f99e3cde10663b2b97d9b47e8f3beb7d9e1
Daniil <zelenko.danil@gmail.com>
2020-07-05 15:47:25 +0300

    Fix null error

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,44 +6,40 @@ export type UserData = {
     user_id: number,
     name: string,
     email: string,
+    error?: string
 }
 
 type userState = {
     userData: UserData,
-    isLoged: boolean | null;
 }
 
 export class App extends Component<{}, userState> {
     componentDidMount() {
-        if (this.state.isLoged) {
+        if (this.state) {
             return;
         }
         let xhr = new XMLHttpRequest();
         xhr.open("GET", "/data/user");
         xhr.send();
         xhr.onload = () => {
-            if (xhr.status === 200) {
-                this.setState({
-                    userData: JSON.parse(xhr.response),
-                    isLoged: true
-                });
-            }
+            this.setState({
+                userData: JSON.parse(xhr.response),
+            });
         }
     }
      
     setUser(userData: UserData): void {
         this.setState({
             userData: userData,
-            isLoged: true
         });
     }
 
     render() {
-        if (this.state.isLoged === null) {
+        if (!this.state) {
             return <LoginPage setUserData={ this.setUser } />;
 
-        } else if (this.state.isLoged === false ) {
-            return <LoginPage error="Data is wrong" setUserData={ this.setUser }/>
+        } else if (this.state.userData.error) {
+            return <LoginPage error={ this.state.userData.error} setUserData={ this.setUser }/>
 
         } else {
             return <Home user={ this.state.userData }/>
