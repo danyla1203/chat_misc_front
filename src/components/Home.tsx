833b918b4ec1310ca1ac41c02f1e53ea8d69c753
Daniil <zelenko.danil@gmail.com>
2020-07-07 21:49:45 +0300

    add socket to Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,23 +2,29 @@ import React, { useState } from "react";
 import { ChatList } from "./ChatList";
 import { UserData } from "./App";
 import { CurrentChat } from "./chat/CurrentChat";
-
+import { UserSocket } from "./lib/UserSocket";
 
 type HomeProps = {
-    user: UserData
+    user: UserData,
+    socket: UserSocket
 }
-
 export function Home(props: HomeProps) {
     const [ currentChat, setChat ] = useState("");
-
+    
     const setCurrentChat = (chatLogin: string) => {
         setChat(chatLogin);
     }
 
     return (
         <div id="home">
-            <ChatList setChat={ setCurrentChat }/>
-            <CurrentChat chat={ currentChat } />
+            <ChatList 
+                setChat={ setCurrentChat }
+                socket={ props.socket }
+            />
+            <CurrentChat 
+                chat={ currentChat } 
+                socket={ props.socket }
+            />
         </div>
     )
 }
