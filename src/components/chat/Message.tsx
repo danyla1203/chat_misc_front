d95bdccaedf36409208dc407519c103b5eb8c3bc
Daniil <zelenko.danil@gmail.com>
2020-07-11 22:18:51 +0300

    write message markup

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -6,9 +6,15 @@ type MessageProps = {
     message: MessageType
 }
 export function Message(props: MessageProps) {
+    let autor = props.message.autor;
+    let text = props.message.text;
     return (
         <div className="message">
-
+            <div className="autor">
+                <img src={ autor.avatar_url } alt="user"/>
+                <h4>{ autor.name }</h4>
+            </div>
+            { text } 
         </div>
     )
 }
